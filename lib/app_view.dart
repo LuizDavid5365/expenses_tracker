22c03943b0e4aa80db5a895083607ac067b19010
Luiz David <luizdavid5365@gmail.com>
2025-04-01 21:38:12 -0300

    first commit

---
+++ lib/app_view.dart
@@ -0,0 +1,26 @@
+import 'package:expenses_tracker/screens/home/views/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyAppView extends StatelessWidget {
+  const MyAppView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Lukay finanças",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100,
+          onSurface: Colors.black,
+          primary: const Color(0xFF00B2E7),
+          secondary: const Color(0xFFE064F7),
+          tertiary: const Color(0xFFFF8D6C),
+          outline: Colors.grey.shade400,
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+ 
